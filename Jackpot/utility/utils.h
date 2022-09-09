a201fe27f11abc6d96bbb5080eee0fdc664a5aa7
Tryliom <alexishaldy@gmail.com>
2022-09-09 10:25:22 +0200

    Add comments to Utils

--- Jackpot/utility/utils.h
+++ Jackpot/utility/utils.h
@@ -1,5 +1,14 @@
 #pragma once
 
+/**
+ * @brief A namespace for utility functions.
+ */
 namespace Utils {
+	/**
+	 * @brief Get a random integer between min and max included.
+	 * @param min The minimum value
+	 * @param max The maximum value
+	 * @return
+	 */
 	int getRandomInt(int min, int max);
 };
